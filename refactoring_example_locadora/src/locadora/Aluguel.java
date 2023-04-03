34f1ff72a73824383307707de0ce330692fb04e0
Sergio Lima Jr <87608071+sergioclimajr@users.noreply.github.com>
2023-04-02 23:51:26 -0300

    Add files via upload

--- refactoring_example_locadora/src/locadora/Aluguel.java
+++ refactoring_example_locadora/src/locadora/Aluguel.java
@@ -4,7 +4,6 @@ public class Aluguel {
 	private Alugavel alugavel;
 	private int diasAlugado;
 	
-	 
 	   public Aluguel(Alugavel alugavel, int diasAlugado) {
 	      this.alugavel = alugavel;
 	      this.diasAlugado = diasAlugado;
